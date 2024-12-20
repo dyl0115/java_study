9658032997300cecd1ea94dc1bb135a2a0d69ffe
doubleDRG <dyl660115@gmail.com>
2024-12-20 12:47:58 +0900

    init

---
+++ src/test/java/중급1/helloEnum/Helloo.java
@@ -0,0 +1,11 @@
+package 중급1.helloEnum;
+
+public class Helloo
+{
+    public static void main(String[] args)
+    {
+        Integer i = 123456;
+        i += 2;
+        System.out.println(i);
+    }
+}
--- src/test/java/중급1/integer_클래스/Integer_테스트.java
+++ src/test/java/중급1/integer_클래스/Integer_테스트.java
@@ -1,13 +1,43 @@
 package 중급1.integer_클래스;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
 
 public class Integer_테스트
 {
     @Test
     public void test()
     {
-        int reverse = Integer.reverse(123456);
-        System.out.println(reverse);
+
+        HashMap<String, Hello> map1 = new HashMap<>();
+        map1.put("key1", new Hello(123456));
+        Hello value1 = map1.get("key1");
+        value1.value += 2;
+
+        System.out.println(map1.get("key1").value);
+
+        HashMap<String, Bye> map2 = new HashMap<>();
+        map2.put("key1", new Bye(123456));
+        Bye value2 = map2.get("key1");
+        value2.value += 2;
+
+        System.out.println(map2.get("key1").value);
+    }
+
+    @AllArgsConstructor
+    @Getter
+    class Hello
+    {
+        int value;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    class Bye
+    {
+        Integer value;
     }
 }
